4d45881604772fb0f554900e4e2d4f1d229f91e6
b1ink <b1ink301@b1inkui-MacBook-Air.local>
2016-08-13 23:03:29 +0900

    bug fixed

--- bin/update.sh
+++ bin/update.sh
@@ -4,3 +4,9 @@ rm -f ../Packages.bz2
 ./dpkg-scanpackages -m .. /dev/null > ../Packages
 bzip2 ../Packages
 
+# gpg -abs -o Release.gpg Release
+# git add --all
+# git commit -m ""
+# git push origin gh-pages
+
+
